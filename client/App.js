cf8596875b2494c10e2f9176f9312a765594554c
bythealphabet <bythealphabet@gmail.com>
2021-02-15 09:42:21 -0400

    striping the M part of Mern to start

--- client/App.js
+++ client/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 const App = () => {
-  return (
-    <div>
-      <h1>Hello Wor d!</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Hello World! this is so cool</h1>
+		</div>
+	);
 };
 export default hot(module)(App);
--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
-import app from "./express";
-import mongoose from "mongoose";
+import path from "path";
+import express from "express";
+import htmlTemplate from "./../htmlTemplate";
+const app = express();
+//////////////////////////////////////////////////////////////////
+////comment the 2 lines below when going to run in production ////
+import devBundle from "../build-utils/devBundle";
 import config from "../config/config";
 
-mongoose.Promise = global.Promise;
-mongoose
-  .connect(config.mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then((result) => {
-    app.listen(config.port);
-    console.log("Server is listening on PORT:", config.port);
-  })
-  .catch((err) => {
-    console.log("Error Mongoose Connection:", err);
-  });
+devBundle.compile(app);
+///////////////////////////////////////////////////////////////
+
+const CURRENT_WORKING_DIR = process.cwd();
+app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
+
+app.get("/", (req, res) => {
+  res.status(200).send(htmlTemplate());
+});
+
+app.listen(config.port);
+console.log("Server is listening on PORT:", config.port);
